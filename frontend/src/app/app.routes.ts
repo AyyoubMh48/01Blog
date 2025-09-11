57a6cda562c4a309f6de8978160c92fe9392a751
Ayyoub Mahfoud <mahfoudayyoub@gmail.com>
2025-09-11 14:30:34 +0100

    create pages and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Admin } from './pages/admin/admin';
+import { Block } from './pages/block/block';
+import { Feed} from './pages/feed/feed';
+import { Login} from './pages/login/login';
+import { PostEditor} from './pages/post-editor/post-editor';
+import { Profile} from './pages/profile/profile';
+import { Register} from './pages/register/register';
+
+export const routes: Routes = [
+    { path: 'login', component: Login},
+    { path: 'register', component: Register},
+    { path: 'feed', component: Feed},
+    { path: 'block/:userId', component: Block},
+    { path: 'post/new', component: PostEditor},
+    { path: 'post/edit/:postId', component: PostEditor},
+    { path: 'profile', component: Profile},
+    { path: 'admin', component: Admin},
+    { path: '', redirectTo: '/feed', pathMatch: 'full' },
+];
