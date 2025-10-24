537331696b3e49423c7c6663a537e3eb34ad03df
Ayyoub Mahfoud <ayoubzone01@gmail.com>
2025-10-24 16:04:00 +0100

    add footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth';
 import { HeaderComponent } from './components/header/header'; 
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
