91c6d8fc72e4bcb285772de9f0f8dedc1beb3c6c
Ayyoub Mahfoud <ayoubzone01@gmail.com>
2025-10-28 17:20:57 +0100

    new ui for edit post need to fix

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -8,12 +8,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterLink,MatCardModule,
     MatFormFieldModule,
-    MatInputModule,
+    MatInputModule,MatIconModule,
     MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -7,13 +7,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterLink, MatCardModule,
     MatFormFieldModule,
-    MatInputModule,
+    MatInputModule,MatIconModule,
     MatButtonModule],
   templateUrl: './register.html',
   styleUrls: ['./register.scss']
