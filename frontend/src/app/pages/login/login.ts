377e10e936851499d783b9540cbf67b49a86976b
Ayyoub Mahfoud <ayoubzone01@gmail.com>
2025-11-05 18:03:58 +0100

    need to fix color dark mod in loging & register

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -9,6 +9,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +19,10 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [CommonModule, FormsModule, RouterLink,MatCardModule,
     MatFormFieldModule,
     MatInputModule,MatIconModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+
     MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
@@ -23,24 +30,41 @@ import { MatIconModule } from '@angular/material/icon';
 export class Login {
 
   errorMessage: string | null = null;
+  showPassword = false;
+  isLoading = false;
+  rememberMe = false;
 
   constructor(
     private authService: AuthService,
     private router: Router,
+    private cdr: ChangeDetectorRef
   ) {}
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
 
   onSubmit(loginForm: NgForm) {
     if (loginForm.invalid) {
-      return; 
+      return;
     }
+
     this.errorMessage = null;
+    this.isLoading = true; 
 
     this.authService.login(loginForm.value).subscribe({
       next: () => {
+        this.isLoading = false; 
+        this.cdr.detectChanges(); 
         this.router.navigate(['/feed']);
       },
       error: (err) => {
-        this.errorMessage = err.error?.message || 'Login failed. Please check your credentials.';
+        this.isLoading = false;
+        this.errorMessage = 
+          err.error?.message || 
+          'Login failed. Please check your credentials.';
+        this.cdr.detectChanges(); 
       }
     });
   }
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -8,24 +8,33 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
 
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterLink, MatCardModule,
-    MatFormFieldModule,
+    MatFormFieldModule,MatCheckboxModule,MatProgressSpinnerModule,
     MatInputModule,MatIconModule,
     MatButtonModule],
   templateUrl: './register.html',
-  styleUrls: ['./register.scss']
+  styleUrl: './register.scss'
 })
 export class Register {
 
   errorMessage: string | null = null; 
   passwordMismatch = false;
+  showPassword = false;
+  isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router,private cdr: ChangeDetectorRef ) {}
+
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
   onSubmit(registerForm: NgForm) {
     this.errorMessage = null;
@@ -41,16 +50,21 @@ export class Register {
       this.passwordMismatch = true;
       return;
     }
+    this.isLoading = true;
 
     this.authService.register(registerForm.value).subscribe({
       next: (response) => {
         console.log('Registration successful!', response);
+        this.isLoading = false;
+         this.cdr.detectChanges();   
         this.router.navigate(['/login']);
       },
       error: (err) => {
         console.error('Full error object:', err);
+        this.isLoading = false;
 
         this.errorMessage = err.error?.message || 'An unknown error occurred.';
+        this.cdr.detectChanges();   
         console.error('Registration failed:', this.errorMessage);
       }
     });
